777dad2b06dc57404ac8d10d9ea9182169ae94fc
placido <placidobb.ac@gmail.com>
2023-08-26 11:08:50 -0300

    Criamos as rotas da aplicacao

--- server.js
+++ server.js
@@ -4,11 +4,19 @@ const http = require('http')
 //definindo uma porta para sevidor nosso servidor
 const port = 3000;
 
+//Criando um objeto chamado rotas 
+const rotas = {
+    '/': 'Curso de Node',
+    '/livros': 'Entrei na pag de livros',
+    '/autores': 'Listagem de autores',
+    '/editora': 'Pagina de editora',
+    '/sobre': 'Info sobre o projeto'
+}
 
 //constante para o servidor e definindo a resposta do servidor
 const server = http.createServer((req, res) => {
 res.writeHead(200, {'Content-Type': 'text/plain'});
-res.end('Curso de Node');
+res.end(rotas[req.url]);
 })
 
 //Definindo a porta que o servidor vai escutar a requisicao
